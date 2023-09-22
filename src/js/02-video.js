1a0572c34a7dd41de948abf3c9e354b5f4d05d39
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2023-09-22 18:14:12 +0300

    Create form handler

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+
+const timeCode = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(timeCode).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            alert("The time was less than 0 or greater than the video’s duration");
+            break;
+
+        default:
+            alert("Some other error occurred");
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 
+import throttle from 'lodash.throttle';
+
+const inputContent = document.querySelector("input");
+const textareaContent = document.querySelector("textarea");
+const submitButton = document.querySelector("button");
+
+const formData = localStorage.getItem("feedback-form-state");
+
+if (formData) { 
+    const obj = JSON.parse(formData);
+    inputContent.value = obj.email;
+    textareaContent.value = obj.message;
+}
+
+function saveFormData(fieldName, fieldValue) {
+    const userForm = localStorage.getItem("feedback-form-state");
+    const obj = !userForm ? {} : JSON.parse(userForm);
+
+    obj[fieldName] = fieldValue;
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+    console.log(obj);
+}
+
+submitButton.addEventListener("click", (event) => { 
+    event.preventDefault();
+    localStorage.removeItem("feedback-form-state");
+    inputContent.value = "";
+    textareaContent.value = "";
+});
+
+inputContent.oninput = throttle((data) => {
+    saveFormData("email", data.target.value);
+}, 500);
+
+textareaContent.oninput = throttle((data) => {
+    saveFormData("message", data.target.value);
+}, 500);
